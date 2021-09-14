15045e66f3942ea1141a3fd1f187fc033c706de7
ThinEi <thineikhaing@gmail.com>
2021-09-15 02:19:42 +0800

    deploy in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-
+  
+  root 'pages#home'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :videos, only: [:index, :show]
-  root 'pages#home'
+  
+  authenticate :user, lambda { |u| u.admin? } do
+    resources :videos
+  end
 
   match '/send_mail', to: 'pages#send_mail', via: 'post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
